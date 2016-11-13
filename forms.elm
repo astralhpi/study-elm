aa52cca2c68d0187d66539a01a0b2a436131e48d
Song Jaehak <master@hpi.cc>
2016-11-13 21:15:16 +0900

    formsㅇ에서 버튼 눌렀을 때 체크하도록 수정

--- forms.elm
+++ forms.elm
@@ -1,9 +1,10 @@
 import Html exposing (..)
 import Html.App as App
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
+import Html.Events exposing (onInput, onClick)
 import String
 import Char
+import Debug
 
 main =
   App.beginnerProgram { model = model, view = view, update = update }
@@ -16,11 +17,12 @@ type alias Model =
   , password : String
   , passwordAgain : String
   , age: String
+  , status: (String, String)
   }
 
 model : Model
 model =
-  Model "" "" "" ""
+  Model "" "" "" "" ("", "")
 
 
 -- UPDATE
@@ -30,6 +32,7 @@ type Msg
   | Password String
   | PasswordAgain String
   | Age String
+  | Submit
 
 
 update : Msg -> Model -> Model
@@ -47,6 +50,9 @@ update msg model =
     Age age ->
       { model | age = age }
 
+    Submit ->
+      { model |  status = viewValidation model }
+
 -- VIEW
 
 view : Model -> Html Msg
@@ -56,26 +62,26 @@ view model =
     , input [ type' "password", placeholder "Password", onInput Password ] []
     , input [ type' "password", placeholder "Re-enter Password", onInput PasswordAgain] []
     , input [ type' "text", placeholder "Age", onInput Age ] []
-    , viewValidation model
+    , button [ onClick Submit ] [ text "submit" ]
+    , let
+          (color, txt) = model.status
+      in
+        div [ style [ ("color", color)] ] [ text txt ]
     ]
 
-viewValidation : Model -> Html Msg
+viewValidation : Model -> (String, String)
 viewValidation model =
-  let
-    (color, message) =
-      if not (String.all Char.isDigit model.age) then
-        ("red", "Age should be number")
-      else if String.length model.password <= 8 then
-        ("red", "Password should be longer than 8 characters!")
-      else if not (String.any Char.isUpper model.password) then
-        ("red", "Password should have upper case")
-      else if not (String.any Char.isLower model.password) then
-        ("red", "Password should have lower case")
-      else if not (String.any Char.isDigit model.password) then
-        ("red", "Password should have digit")
-      else if not (model.password == model.passwordAgain) then
-        ("red", "Passwords do not match!")
-      else
-        ("green", "OK")
-  in
-    div [ style [("color", color)] ] [ text message ]
+  if not (String.all Char.isDigit model.age) then
+    ("red", "Age should be number")
+  else if String.length model.password <= 8 then
+    ("red", "Password should be longer than 8 characters!")
+  else if not (String.any Char.isUpper model.password) then
+    ("red", "Password should have upper case")
+  else if not (String.any Char.isLower model.password) then
+    ("red", "Password should have lower case")
+  else if not (String.any Char.isDigit model.password) then
+    ("red", "Password should have digit")
+  else if not (model.password == model.passwordAgain) then
+    ("red", "Passwords do not match!")
+  else
+    ("green", "OK")
