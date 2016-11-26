67056a8d46f2c6bd422b208fc6010b118f34ae5b
Song Jaehak <master@hpi.cc>
2016-11-26 23:41:14 +0900

    add pause button at time-sample.elm

--- time-sample.elm
+++ time-sample.elm
@@ -1,4 +1,5 @@
-import Html exposing (Html)
+import Html exposing (Html, button, div)
+import Html.Events exposing (onClick)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Time exposing (Time, second)
@@ -13,30 +14,40 @@ main =
 
 -- MODEL
 
-type alias Model = Time
+type alias Model =
+  { time: Time
+  , isPaused: Bool
+  }
 
 init : (Model, Cmd Msg)
 init =
-  (0, Cmd.none)
+  (Model 0 False, Cmd.none)
 
 
 -- UPDATE
 
 type Msg
   = Tick Time
+  | Pause
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     Tick newTime ->
-      (newTime, Cmd.none)
+      ({ model | time = newTime }, Cmd.none)
+    Pause ->
+      ({ model | isPaused = True }, Cmd.none)
+      
 
 
 -- SUBSCRIPTIONS
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Time.every second Tick
+  if model.isPaused then
+    Sub.none
+  else
+    Time.every second Tick
 
 -- VIEW
 
@@ -45,7 +56,7 @@ view: Model -> Html Msg
 view model =
   let
     angle =
-      turns (Time.inMinutes model)
+      turns (Time.inMinutes model.time)
 
     handX =
       toString (50 + 40 * cos angle)
@@ -53,7 +64,10 @@ view model =
     handY =
       toString (50 + 40 * sin angle)
   in
-    svg [ viewBox "0 0 100 100", width "300px" ]
-      [ circle [ cx "50", cy "50", r "45", fill "#0B79CE"] []
-      , line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963"] []
+    div []
+      [ svg [ viewBox "0 0 100 100", width "300px" ]
+        [ circle [ cx "50", cy "50", r "45", fill "#0B79CE"] []
+        , line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963"] []
+        ]
+      , button [ onClick Pause ] [ Html.text "pause" ]
       ]
