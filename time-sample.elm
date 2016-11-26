63e3eef6a6035e2dd572dcc7ca8faf4733b2f5dd
Song Jaehak <master@hpi.cc>
2016-11-27 00:35:37 +0900

    add hour and minute hands

--- time-sample.elm
+++ time-sample.elm
@@ -3,6 +3,8 @@ import Html.Events exposing (onClick)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Time exposing (Time, second)
+import Tuple
+import Debug
 
 main =
   Html.program
@@ -55,19 +57,39 @@ view: Model -> Html Msg
 
 view model =
   let
-    angle =
-      turns (Time.inMinutes model.time)
+    correctedTime = model.time - (3 * 60 * 60 * 1000)
+    minutes =
+      (Time.inMinutes correctedTime)
 
-    handX =
-      toString (50 + 40 * cos angle)
+    hours =
+      (Time.inHours correctedTime)
+    
+    halfOfDays =
+      hours / 12
 
-    handY =
-      toString (50 + 40 * sin angle)
+
   in
     div []
       [ svg [ viewBox "0 0 100 100", width "300px" ]
         [ circle [ cx "50", cy "50", r "45", fill "#0B79CE"] []
-        , line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963"] []
+        , hand "#FFFFFF" 40 minutes
+        , hand "#023963" 40 hours
+        , hand "#000000" 30 halfOfDays
         ]
       , button [ onClick Pause ] [ Html.text "pause" ]
       ]
+
+hand: String -> Float -> Float -> Html Msg
+
+hand color length value =
+  let
+    angle =
+      turns (value - 0.25)
+
+    handX =
+      toString (50 + length * cos angle)
+
+    handY =
+      toString (50 + length * sin angle)
+  in
+    line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke color] []
